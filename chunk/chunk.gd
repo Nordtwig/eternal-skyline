b3884abf28893f1e9af06523a2644d47a730abec
Noah <nordqvist.gaming@gmail.com>
2024-03-05 16:42:58 +0100

    generate buildings and rings in chunk

--- chunk/chunk.gd
+++ chunk/chunk.gd
@@ -22,15 +22,38 @@ func _ready() -> void:
 
 
 func add_buildings() -> void:
-    pass
+    for side in [-1, 1]:
+        var z_position = -10
+        for i in 18:
+            if randf() > 0.75:
+                z_position -= randi_range(5, 10)
+                continue
+            var new_building = buildings[randi_range(0, buildings.size()-1)].instantiate()
+            add_child(new_building)
+            new_building.transform.origin.z = z_position
+            new_building.transform.x = 20 * side
+            z_position -= new_building.get_node("MeshInstance3D").mesh.get_aabb().size.z
+
 
 
 func add_obstacle_buildings() -> void:
-    pass
+    if level > 0:
+        for z in range(0, -200, -20):
+            if randf() > 0.8:
+                var new_building = buildings[0].instantiate()
+                add_child(new_building)
+                new_building.position.z = z
+                new_building.position.x += 8
+                new_building.rotation.y = PI / 2
 
 
 func add_rings()  -> void:
-    pass
+    for z in range(0, -200, -10):
+        if randf() > 0.76:
+            var new_ring = ring.instantiate()
+            new_ring.position.z = z
+            new_ring.position.y = randf_range(3, 17)
+            add_child(new_ring)
 
 
 func on_vos_notifier_screen_exited() -> void:
